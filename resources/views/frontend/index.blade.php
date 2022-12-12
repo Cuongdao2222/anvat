b416d38b4fd131a3e9d4df7d6180b21958521937
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-12-12 10:09:30 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -600,7 +600,7 @@ if(!empty($deal)){
                                                         </div>
                                                     </div>
                                                     <button class="btn-add-to-cart n-btn " title="Bấm để thêm vào giỏ hàng" onclick="addToCart({{ $value->product_id }})">
-                                                        <div style="position: relative; z-index: 3;"><img src="./public/react/Images/Icons/plus-filled-red.svg"></div>
+                                                        <div style="position: relative; z-index: 3;"><img src="./public/react/images/icons/plus-filled-red.svg"></div>
                                                     </button>
                                                 </div>
                                             </div>
