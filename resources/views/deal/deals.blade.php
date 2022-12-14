d8d97647d405cef4a995d7935c0dec2a00cd9fc3
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-12-14 15:09:00 +0700

    update

--- resources/views/deal/deals.blade.php
+++ resources/views/deal/deals.blade.php
@@ -311,14 +311,16 @@
 
                                
                             ?>
+
+                             <?php 
+                                $product_info = App\Models\product::find($val->product_id);
+                            ?>
+
+                            @if(!empty($product_info->Price))
                             <tr id="row_1208">
                                 <td>{{ $k }}</td>
 
-                                <?php 
-                                    $product_info = App\Models\product::find($val->product_id);
-                                ?>
-
-                                @if(!empty($product_info->Price))
+                               
                                 <td align="center">
                                     <img src="{{ !empty($product_info->Image)?asset($product_info->Image):'' }}" width="100" alt="{{ $val->name }}">
                                     <!--<div><a style="color:green" href="javascript:;" onclick="delete_special(1208)">Xóa bỏ</a></div>-->
@@ -381,10 +383,11 @@
                                     <a href="javascript:void(0)" onclick="setTimeDeal({{ $val->id }})">Sửa</a>
                                 </td>
 
-                                @endif
+                                
 
 
                             </tr>
+                            @endif
                             @endforeach
 
                             @endisset
