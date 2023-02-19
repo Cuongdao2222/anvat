bbae8929192b3b0d5ff260bb9106c5fa1dd2235f
cuong <cuongdao2692@gmail.com>
2023-02-19 12:54:41 +0700

    update

--- resources/views/frontend/test.blade.php
+++ resources/views/frontend/test.blade.php
@@ -9,11 +9,27 @@
 
 
 <head>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" />
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+  <!-- Add jQuery library -->
+  <script type="text/javascript" src="{{ asset('lib/jquery-1.10.1.min.js')}}"></script>
+
+  <!-- Add mousewheel plugin (this is optional) -->
+  <script type="text/javascript" src="{{ asset('lib/jquery.mousewheel-3.0.6.pack.js')}}"></script>
+
+  <!-- Add fancyBox main JS and CSS files -->
+  <script type="text/javascript" src="{{ asset('source/jquery.fancybox.js?v=2.1.5')}}"></script>
+  <link rel="stylesheet" type="text/css" href="{{ asset('source/jquery.fancybox.css?v=2.1.5')}}" media="screen" />
+
+  <!-- Add Button helper (this is optional) -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('source/helpers/jquery.fancybox-buttons.css?v=1.0.5')}}" />
+  <script type="text/javascript" src="{{ asset('source/helpers/jquery.fancybox-buttons.js?v=1.0.5')}}"></script>
+
+  <!-- Add Thumbnail helper (this is optional) -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('source/helpers/jquery.fancybox-thumbs.css?v=1.0.7')}}" />
+  <script type="text/javascript" src="{{ asset('source/helpers/jquery.fancybox-thumbs.js?v=1.0.7')}}"></script>
+
+  <!-- Add Media helper (this is optional) -->
+  <script type="text/javascript" src="{{ asset('source/helpers/jquery.fancybox-media.js?v=1.0.6')}}"></script>
+
 </head>
 
 <div class="owl-carousel">
