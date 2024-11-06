d5f76ca6e76eba67d1d15c002f9855048c3e0d40
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-06 20:55:25 +0700

    Update details.blade.php

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -126,6 +126,14 @@
                                 <span class="row-2"></span>
                             </button>
 
+                             <button class="add-item-wrapper n-btn btn-add-to-cart btn-add-to-collection ">
+                                <span class="row-1">
+                                    <div class="icon-total-save"><img class="icon" src="https://img.icons8.com/material-outlined/2x/sell-stock.png"></div>
+                                    <span class="text display-block" style="color: rgb(172, 172, 172);" onclick="addToCartRedirect({{ $data->id }})">Đến nơi bán</span>
+                                </span>
+                                <span class="row-2"></span>
+                            </button>
+
 
                             <button class="add-item-wrapper n-btn btn-add-to-cart btn-add-to-collection save">
                                 <span class="row-1">
