acf66c1000217779f32d083e62f57903b661ea0d
cuong12121 <cuongdao12121@gmail.com>
2024-11-13 15:11:56 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -721,13 +721,7 @@ if(!empty($deal)){
                                                                     <div class="Gt3dn4"><span class="ZnuQ4w zbggAt I9vX2T">₫</span><strong class="Rgk_yn">356.000</strong></div>
                                                                 </div>
                                                             </div>
-                                                            <div class="VE_mfV">
-                                                                <div class="g0LhW7" style="height: 16px;">
-                                                                    <div class="Qeov7Y DqPl63">Đã bán 45</div>
-                                                                    <div class="gsxwEM" style="width: 15%; border-radius: 8px 0px 0px 8px;"></div>
-                                                                    <div class="efwV9O" style="border-radius: 8px;"></div>
-                                                                </div>
-                                                            </div>
+                                                            
                                                         </div>
                                                         
                                                     </div>
