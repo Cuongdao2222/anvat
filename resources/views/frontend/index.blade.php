24d795ed90a9bafbb1a1c87489f36837e9f817c6
cuong12121 <cuongdao12121@gmail.com>
2024-11-13 14:06:00 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -612,6 +612,80 @@ if(!empty($deal)){
                                             @foreach($product_sale as  $value)
                                             @if($value->active==1)
 
+                                            <div class="sny52Q">
+                                                <div class="zs1fUu">
+                                                    <div class="UeJ6lG">BẢNG PHẤN MẮT LEMONADE AESTHETIC EYESHADOW PALETTE VERSION 2 20.8G</div>
+                                                    <div class="fD85zn">
+                                                        <div class="d6cl1K">
+                                                            <div class="rUjRDX typo-r10">
+                                                                <div class="A90KS0">
+                                                                    <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid t7jHjH">
+                                                                        <path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path>
+                                                                    </svg>
+                                                                    <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid t7jHjH">
+                                                                        <path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path>
+                                                                    </svg>
+                                                                    <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid t7jHjH">
+                                                                        <path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path>
+                                                                    </svg>
+                                                                    <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid t7jHjH">
+                                                                        <path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path>
+                                                                    </svg>
+                                                                    <div class="t7jHjH">
+                                                                        <div class="p7bZMY" style="width: 94.4689%;">
+                                                                            <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating-solid ysyPe_">
+                                                                                <path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path>
+                                                                            </svg>
+                                                                        </div>
+                                                                        <svg enable-background="new 0 0 15 15" viewBox="0 0 15 15" role="img" class="stardust-icon stardust-icon-rating Pn2N1H">
+                                                                            <path d="m7.5.8l2.2 4.6 4.8.5-3.8 3.2 1.1 5.1-4.3-2.6-4.3 2.6 1.1-5.1-3.8-3.2 4.8-.5z" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path>
+                                                                        </svg>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="_t8Iel">
+                                                        <div class="iVyZmn">
+                                                            <div class="y1PVWz">
+                                                                <div class="I99cV_ hYuPX1 I9vX2T U0tMPz">
+                                                                    <div class="Gt3dn4"><span class="ZnuQ4w hYuPX1 I9vX2T">₫</span><strong class="Rgk_yn">419.000</strong></div>
+                                                                </div>
+                                                                <div class="hcYtZZ ph4Z1T">
+                                                                    <div class="DCjeTh a4s1bT">
+                                                                        <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                            <path d="M9.23077 0H4.23077L0 7.82222L3.5 9.14286V16L10 5.68889L6.53846 4.62222L9.23077 0Z" fill="url(#paint0_linear_2216_10611)"></path>
+                                                                            <defs>
+                                                                                <linearGradient id="paint0_linear_2216_10611" x1="0" y1="0" x2="0" y2="16" gradientUnits="userSpaceOnUse">
+                                                                                    <stop stop-color="#EE4D2D"></stop>
+                                                                                    <stop offset="1" stop-color="#FF7337"></stop>
+                                                                                </linearGradient>
+                                                                            </defs>
+                                                                        </svg>
+                                                                    </div>
+                                                                    -15%
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div class="I99cV_ zbggAt I9vX2T">
+                                                                    <div class="Gt3dn4"><span class="ZnuQ4w zbggAt I9vX2T">₫</span><strong class="Rgk_yn">356.000</strong></div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="VE_mfV">
+                                                                <div class="g0LhW7" style="height: 16px;">
+                                                                    <div class="Qeov7Y DqPl63">Đã bán 45</div>
+                                                                    <div class="gsxwEM" style="width: 15%; border-radius: 8px 0px 0px 8px;"></div>
+                                                                    <div class="efwV9O" style="border-radius: 8px;"></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="r_gprR">
+                                                            <div class="OB_WG4 NUjI4x epiqlJ _1NeKaL">Mua ngay</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
                                             <div class="product-basic-info ">
 
                                                 
